cb274bdd95e9fec6aa8da030fdcd18cc6fb637da
pythonCat <pythoncat@qq.com>
2018-09-19 22:55:46 +0800

    update mysql.sql

--- mysql/mysql.sql
+++ mysql/mysql.sql
@@ -10,6 +10,12 @@ desc chat;
 
 select chat_title,chat_author from chat;
 
-# MySQL 的 WHERE 子句的字符串比较是不区分大小写的。 你可以使用 BINARY 关键字来设定 WHERE 子句的字符串比较是区分大小写的。
+-- MySQL 的 WHERE 子句的字符串比较是不区分大小写的。 你可以使用 BINARY 关键字来设定 WHERE 子句的字符串比较是区分大小写的。
 
 select chat_title from chat where chat_author='比亚迪车主' limit 100;
+
+-- sql 语句分类：
+
+-- DDL : data definition language ==> 用于对 database table column 的增删改查操作。
+-- DML: data manipulation language ===> 用于对 table 中的数据进行 增删改操作。
+-- DQL: data query language ===> 用于对 table 中的数据进行 查询操作。
